575598b573b42802a7f3f098f53b7df3a93987a1
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-05-28 19:01:08 -0300

    ref: update bot to use main

--- lesson-1/bot.py
+++ lesson-1/bot.py
@@ -6,7 +6,7 @@ import sys
 import random
 import time
 
-def signal_handler(sig, frame):
+def signal_handler(client, sig=None, frame=None):
     try:
         client.send("exit".encode())
         client.close()
@@ -15,24 +15,7 @@ def signal_handler(sig, frame):
     finally:
         sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
-
-parser = argparse.ArgumentParser(description='TCP Chat Client')
-parser.add_argument('client', help='Client name')
-parser.add_argument('--host', default='0.0.0.0', help='Host IP address (default: 0.0.0.0)')
-parser.add_argument('--port', type=int, default=65432, help='Port number (default: 65432)')
-parser.add_argument('-n', type=int, default=100, help='Total number of messages')
-args = parser.parse_args()
-
-nickname = args.client
-HOST     = args.host
-PORT     = args.port
-N        = args.n
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
-
-def receive():
+def receive(client):
     while True:
         try:
             message = client.recv(1024).decode()
@@ -45,19 +28,36 @@ def receive():
             client.close()
             break
 
-def write(n:int=10):
+def write(client, nickname, n:int=10, base:int=5):
     for i in range(n):
         message = f"{nickname}: {i+1}/{n}"
         client.send(message.encode())
-        base = 5
         random_dur = random.uniform(0, 1) # seconds
         time.sleep(base + random_dur)
 
     client.send("exit".encode())
     client.close()
 
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='TCP Chat Client')
+    parser.add_argument('client', help='Client name')
+    parser.add_argument('--host', default='0.0.0.0', help='Host IP address (default: 0.0.0.0)')
+    parser.add_argument('--port', type=int, default=65432, help='Port number (default: 65432)')
+    parser.add_argument('-n', type=int, default=100, help='Total number of messages')
+    args = parser.parse_args()
 
-write_thread = threading.Thread(target=write, args=(N,))
-write_thread.start()
+    nickname = args.client
+    HOST     = args.host
+    PORT     = args.port
+    N        = args.n
+
+    signal.signal(signal.SIGINT, lambda: signal_handler(client))
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
+
+    receive_thread = threading.Thread(target=receive, args=(client,))
+    receive_thread.start()
+
+    write_thread = threading.Thread(target=write, args=(client, nickname, N,))
+    write_thread.start()
